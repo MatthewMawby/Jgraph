0e1a6ba602da87b6c82a85a29de1f09b1b63b894
MatthewMawby <themesta@gmail.com>
2016-10-15 15:40:13 -0400

    implemented Edge class

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -2,3 +2,25 @@
 * @author Matthew Mawby
 * @version 1.0
 */
+
+package jgraph;
+
+public class Edge<N,E> {
+
+    //member variables are for JGraph use only
+    protected N from;
+    protected N to;
+    protected E label;
+
+    //@param: N from  | the label of the node the edge is coming from
+    //@param: N to    | the label of the node the edge is going to
+    //@param: E label | the label of the edge
+    //@throws: none
+    //@returns: this (a new edge)
+    public Edge(N to, N from, E label)
+    {
+        this.to = to;
+        this.from = from;
+        this.label = label;
+    }
+}
--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -12,12 +12,12 @@ public interface Graph<N,E> {
         //@returns:  true if the node was added, false if otherwise
         public boolean add_node(N name);
 
-        //@param: N from  | the label of the node the edge is coming from
         //@param: N to    | the label of the node the edge is going to
+        //@param: N from  | the label of the node the edge is coming from
         //@param: E label | the label of the edge
         //@throws: none
         //@returns:  true if the edge was added, false if otherwise
-        public boolean add_edge(N from, N to, E label);
+        public boolean add_edge(N to, N from, E label);
 
         //other functions such as search/contains
 }
